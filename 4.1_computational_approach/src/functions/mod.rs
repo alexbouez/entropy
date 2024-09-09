8554ebd82bfc693e72312cb568d508821c39e290
abouez <alexandre.bouez@ru.nl>
2024-09-09 14:09:18 +0200

    Cleaned

--- 4.1_computational_approach/src/functions/mod.rs
+++
@@ -1,9 +0,0 @@
-#![allow(non_snake_case)]
-//! Entropy Gathering - Functions
-//!
-//! This module gives access to a set of `unsigned int'-specific functions (shift, not...).
-
-/// Module implementing bitwise operations for 16-bit registers. 
-pub mod u16;
-/// Module implementing bitwise operations for 32-bit registers. 
-pub mod u32;
